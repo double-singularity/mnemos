c713c6b67aa5e93cf3a8e6cf51c4769f4746e66b
panzer <rstu1081@gmail.com>
2026-03-12 23:00:20 +0700

    refactor: change mod.rs using newer conventions

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
+pub mod types {
+    pub mod vector;
+}
+
+pub mod storage {
+    pub mod vector_store;
+}
+
 pub mod distance {
     pub mod metrics;
 }
 
-pub mod types;
-pub mod storage;
+pub mod index {
+    pub mod knn;
+}
+
+// pub mod db; 
